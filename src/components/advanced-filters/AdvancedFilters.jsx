9c0bfbf4b1ff11bdfc29a61d2d3443af3a727cc0
Dan Garro <dobetterwithdan@gmail.com>
2024-08-16 11:09:25 -0400

    created advanced filter icons and started component

---
+++ src/components/advanced-filters/AdvancedFilters.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import dietaryRestrictions from "../../../public/diet-restrictions-icon.png";
+import servesAlcohol from "../../../public/alcohol-icon.png";
+import evCharge from "../../../public/ev-charge-icon.png";
+import outdoorSeating from "../../../public/outdoor-seating-icon.png";
+import liveMusic from "../../../public/live-music-icon.png";
+import petFriendly from "../../../public/pet-friendly-icon.png";
+import familyFriendly from "../../../public/family-friendly-icon.png";
+import parking from "../../../public/parking-icon.png";
+import payments from "../../../public/payment-icon.png";
+import coffee from "../../../public/coffee-icon.png";
+import wifiIcon from "../../../public/wifi-icon.png";
+
+const infoAdvancedFilters = [
+  {
+    id: 1,
+    icon: dietaryRestrictions,
+    description: "Dietary restrictions and food allergies",
+  },
+  {
+    id: 2,
+    icon: servesAlcohol,
+    description: "Serves alcohol",
+  },
+  {
+    id: 3,
+    icon: evCharge,
+    description: "EV charge station",
+  },
+  {
+    id: 4,
+    icon: outdoorSeating,
+    description: "Outdoor seating",
+  },
+  {
+    id: 5,
+    icon: liveMusic,
+    description: "Live music",
+  },
+  {
+    id: 6,
+    icon: dietaryRestrictions,
+    description: "Dietary restrictions and food allergies",
+  },
+  {
+    id: 7,
+    icon: petFriendly,
+    description: "Pet friendly",
+  },
+  {
+    id: 8,
+    icon: familyFriendly,
+    description: "Family friendly",
+  },
+  {
+    id: 9,
+    icon: parking,
+    description: "Parking",
+  },
+  {
+    id: 10,
+    icon: payments,
+    description: "Payment options",
+  },
+  {
+    id: 11,
+    icon: coffee,
+    description: "Serves coffee",
+  },
+  {
+    id: 12,
+    icon: wifiIcon,
+    description: "Wifi access",
+  },
+];
