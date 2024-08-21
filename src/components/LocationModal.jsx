6e836d6cc6edeb1b35232a0d754fdf0db39fe9de
Randall <108246257+randalltr@users.noreply.github.com>
2024-08-21 10:19:21 +0700

    added location input and submit button

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -1,26 +1,46 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 
 const LocationModal = () => {
+  const [location, setLocation] = useState("");
+
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log("Location:", location);
+  };
+
   return (
     <div>
       {/* Open the modal using document.getElementById('ID').showModal() method */}
       <button
         className="btn"
-        onClick={() => document.getElementById("my_modal_1").showModal()}
+        onClick={() => document.getElementById("location-modal").showModal()}
       >
-        update location
+        Update Location
       </button>
 
-      <dialog id="my_modal_1" className="modal">
+      <dialog id="location-modal" className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Update My Location</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
+          <h3 className="font-bold text-lg">Enter Your Location</h3>
+          <p className="py-4">Please enter your location below.</p>
+          <input
+            type="text"
+            placeholder="Enter your location"
+            value={location}
+            onChange={handleLocationChange}
+            className="p-2"
+          />
+
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
+              <button className="btn m-2" onClick={handleSubmit}>
+                Submit
+              </button>
               <button className="btn">Close</button>
             </form>
           </div>
