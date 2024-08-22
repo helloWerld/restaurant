0f6045305e68c44dad27568ec4cb8616114ed2cb
Dan Garro <dobetterwithdan@gmail.com>
2024-08-22 16:07:02 -0400

    added dropdown for multi-option filter

--- src/components/advanced-filters/AdvancedFilters.jsx
+++ src/components/advanced-filters/AdvancedFilters.jsx
@@ -72,7 +72,20 @@ const filtersWithOptions = [
     id: 1,
     icon: payments,
     description: "Payment options",
-    hasOptions: ["card", "cash", "Venmo"],
+    hasOptions: [
+      {
+        icon: cardPayment,
+        description: "Card",
+      },
+      {
+        icon: cashPayment,
+        description: "Cash",
+      },
+      {
+        icon: mobilePayment,
+        description: "Mobile",
+      },
+    ],
   },
 ];
 
@@ -90,33 +103,31 @@ const AdvancedFilters = () => {
           </button>
         </div>
       ))}
-      {filtersWithOptions.map((filter) => {
-        <div key={filter.id} className="tooltip" data-tip={filter.description}>
-          <div className="dropdown dropdown-hover">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-circle btn-ghost"
-            >
-              <img
-                className="h-6 w-6"
-                src={filter.icon.src}
-                alt={filter.description}
-              />
-            </div>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
-            >
-              {filter.hasOptions.map((option, index) => (
-                <li key={index}>
-                  <a>{option}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>;
-      })}
+      <div className="tooltip" data-tip={filtersWithOptions[0].description}>
+        <details className="dropdown">
+          <summary className="btn btn-circle btn-ghost">
+            <img
+              className="h-6 w-6"
+              src={filtersWithOptions[0].icon.src}
+              alt={filtersWithOptions[0].description}
+            />
+          </summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            {filtersWithOptions[0].hasOptions.map((option, index) => (
+              <li key={index}>
+                <a>
+                  <img
+                    className="h-6 w-6"
+                    src={option.icon.src}
+                    alt={option.description}
+                  />{" "}
+                  {option.description}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </details>
+      </div>
     </div>
   );
 };
