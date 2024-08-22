2b8a3ab2c2ea40d861f37a1507a6ca6d0e540ae9
helloWerld <108149462+helloWerld@users.noreply.github.com>
2024-08-22 19:32:21 -0400

    hotfix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,39 @@
+'use client';
+
 import React from 'react';
 import logo from '../../public/icon.png';
 import Link from 'next/link';
 
 const Navbar = () => {
+	const searchRestaurants = async (e) => {
+		e.preventDefault();
+		console.log('Searching...');
+		const responseData = await fetch('/api/places', {
+			// prettier-ignore
+			"textQuery": 'pizza shop in wellington fl',
+		});
+		//const data = await responseData.json();
+		console.log('response data', responseData.json());
+	};
 	return (
 		<div className="navbar bg-primary px-4">
 			<Link href="/" className="flex-1 text-primary-content  font-medium">
 				<img src={logo.src} className="w-10 h-10" /> Yum Finder
 			</Link>
 			<div className="flex-none gap-2">
-				<div className="hidden md:form-control">
+				<form
+					onSubmit={searchRestaurants}
+					className="md:flex flex-row join hidden"
+				>
 					<input
 						type="text"
 						placeholder="Search for a Restaurant"
-						className="input input-bordered w-80"
+						className="join-item input input-bordered w-80"
 					/>
-				</div>
+					<button type="submit" className="btn btn-secondary join-item">
+						Search
+					</button>
+				</form>
 				<div className="dropdown dropdown-end z-[100]">
 					<div
 						tabIndex={0}
