38fff66bde12fa9c2247fbd993db6ea34422a01b
helloWerld <108149462+helloWerld@users.noreply.github.com>
2024-09-13 12:11:04 -0400

    more cleanup, removed extra Hero import

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
-import Hero from "@/components/homepage/Hero";
-import Hero from "@/components/homepage/Hero";
+import Hero from '@/components/homepage/Hero';
 
 export default function Home() {
 	return (
