2a2a64c05f43b5977e527a174b07a37aef6cc11b
Dan Garro <dobetterwithdan@gmail.com>
2024-09-17 19:15:50 -0400

    updated metadata - added opengraph image, robots.js, sitemap.js, and manifest.json

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,15 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Navbar from '@/components/ui/Navbar';
-import Footer from '@/components/ui/Footer';
-import { AppContextProvider } from '@/context/context-provider';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
+import { AppContextProvider } from "@/context/context-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  openGraph: {
+    images: "/opengraph-image.png",
+  },
   title: {
     template: "%s | Restaurant Finder",
     default: "Restaurant Finder | Find somewhere to eat without the hassle",
@@ -16,15 +19,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<AppContextProvider>
-				<body className={`${inter.className} min-h-screen w-full`}>
-					<Navbar />
-					{children}
-					<Footer />
-				</body>
-			</AppContextProvider>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <AppContextProvider>
+        <body className={`${inter.className} min-h-screen w-full`}>
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </AppContextProvider>
+    </html>
+  );
 }
---
+++ src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://https://restaurant-5e91b.web.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://https://restaurant-5e91b.web.app/spin",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://https://restaurant-5e91b.web.app/dashboard",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://https://restaurant-5e91b.web.app/details",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://https://restaurant-5e91b.web.app/search",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://https://restaurant-5e91b.web.app/auth",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
